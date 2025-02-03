3b3458ddc5cd805bec104036cd7516a3a304ae15
Lido Paglia <lido@paglia.org>
2025-02-03 10:58:27 -0500

    use catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,72 +1,24 @@
+-- https://github.com/catppuccin/nvim
+-- https://catppuccin.com/palette
 return {
-
   {
-    "erikbackman/brightburn.vim",
-    enabled = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "brightburn"
-      vim.api.nvim_set_hl(0, "Comment", { fg = "#585858", italic = true })
-    end,
-  },
-  {
-    "rose-pine/neovim",
-    enabled = false,
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup {
-        disable_background = true,
-      }
-      vim.cmd.colorscheme "rose-pine"
-    end,
-  },
-
-  {
-    "p00f/alabaster.nvim",
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    config = function(_)
-      vim.cmd.colorscheme "alabaster"
-    end,
-  },
-
-  {
-    "xiyaowong/transparent.nvim",
-    enabled = true,
-    lazy = false,
-    config = {
-      extra_groups = {
-        "FloatBorder",
-        "LSPInfoBorder",
-        "NeoTreeNormal",
-        "NeoTreeNormalNC",
-        "NormalFloat",
-        "NotifyDEBUGBody",
-        "NotifyDEBUGBorder",
-        "NotifyERRORBody",
-        "NotifyERRORBorder",
-        "NotifyINFOBody",
-        "NotifyINFOBorder",
-        "NotifyTRACEBody",
-        "NotifyTRACEBorder",
-        "NotifyWARNBody",
-        "NotifyWARNBorder",
-        "TelescopeBorder",
-        "TelescopePreviewNormal",
-        "TelescopePromptNormal",
-        "TelescopeResultsNormal",
-        "WhichKeyFloat",
-      },
-      exclude_groups = {},
-    },
-    keys = {
-      {
-        "<Leader>tt",
-        "<Cmd>TransparentToggle<Cr>",
-        desc = "Toggle Transparency",
+    opts = {
+      transparent_background = true,
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
       },
     },
+    config = function()
+      -- set the colorscheme
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 }
