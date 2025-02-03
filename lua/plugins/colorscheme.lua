52f692fd5e3ff1e9282caa82988b0cb40685201b
Lido Paglia <lido@paglia.org>
2025-02-03 16:43:24 -0500

    add transparent toggle

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -21,4 +21,40 @@ return {
       vim.cmd.colorscheme("catppuccin")
     end,
   },
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    config = {
+      extra_groups = {
+        "FloatBorder",
+        "LSPInfoBorder",
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+        "NormalFloat",
+        "NotifyDEBUGBody",
+        "NotifyDEBUGBorder",
+        "NotifyERRORBody",
+        "NotifyERRORBorder",
+        "NotifyINFOBody",
+        "NotifyINFOBorder",
+        "NotifyTRACEBody",
+        "NotifyTRACEBorder",
+        "NotifyWARNBody",
+        "NotifyWARNBorder",
+        "TelescopeBorder",
+        "TelescopePreviewNormal",
+        "TelescopePromptNormal",
+        "TelescopeResultsNormal",
+        "WhichKeyFloat",
+      },
+      exclude_groups = {},
+    },
+    keys = {
+      {
+        "<Leader>tt",
+        "<Cmd>TransparentToggle<Cr>",
+        desc = "Toggle Transparency",
+      },
+    },
+  },
 }
