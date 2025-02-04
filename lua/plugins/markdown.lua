619990af5c0a29ed8880f74bc7f04144b9c277ba
Lido Paglia <lido@paglia.org>
2025-02-03 21:19:46 -0500

    add render-markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -10,6 +10,55 @@ return {
     end,
   },
 
+  {
+    -- https://github.com/MeanderingProgrammer/render-markdown.nvim
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      bullet = {
+        enabled = true,
+        icons = { "●", "○", "◆", "◇" },
+        ordered_icons = function(level, index, value)
+          value = vim.trim(value)
+          local value_index = tonumber(value:sub(1, #value - 1))
+          return string.format("%d.", value_index > 1 and value_index or index)
+        end,
+        left_pad = 0,
+        right_pad = 0,
+        highlight = "RenderMarkdownBullet",
+      },
+      checkbox = {
+        enabled = true,
+        position = "inline",
+        unchecked = {
+          icon = "󰄱 ",
+          highlight = "RenderMarkdownUnchecked",
+          scope_highlight = nil,
+        },
+        checked = {
+          icon = "󰱒 ",
+          highlight = "RenderMarkdownChecked",
+          scope_highlight = nil,
+        },
+      },
+      html = {
+        -- Turn on / off all HTML rendering
+        enabled = true,
+        comment = {
+          -- Turn on / off HTML comment concealing
+          conceal = false,
+        },
+      },
+      heading = {
+        sign = false,
+        icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+        position = "inline",
+      },
+    },
+  },
+
   -- "folke/twilight.nvim",
   -- "folke/zen-mode.nvim",
   -- "preservim/vim-pencil",
