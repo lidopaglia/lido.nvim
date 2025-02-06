7fcc0d459604ef1dbb7390b8b1438fe61993b411
Lido Paglia <lido@paglia.org>
2025-02-06 13:30:33 -0500

    enable obsidian plugin

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -52,6 +52,7 @@ return {
         },
       },
       heading = {
+        enabled = false,
         sign = false,
         icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
         position = "inline",
@@ -63,32 +64,29 @@ return {
   -- "folke/zen-mode.nvim",
   -- "preservim/vim-pencil",
 
-  -- {
-  --   -- https://github.com/epwalsh/obsidian.nvim
-  --   "epwalsh/obsidian.nvim",
-  --   ebabled = true,
-  --   version = "*", -- recommended, use latest release instead of latest commit
-  --   lazy = true,
-  --   ft = "markdown",
-  --   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  --   -- event = {
-  --   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   --   "BufReadPre path/to/my-vault/**.md",
-  --   --   "BufNewFile path/to/my-vault/**.md",
-  --   -- },
-  --   dependencies = { "nvim-lua/plenary.nvim" },
-  --   opts = {
-  --     workspaces = {
-  --       {
-  --         name = "personal",
-  --         path = "~/Documents/vaults/notes",
-  --       },
-  --       {
-  --         name = "work",
-  --         path = "~/Documents/vaults/work",
-  --       },
-  --     },
-  --   },
-  -- }
+  {
+    -- https://github.com/epwalsh/obsidian.nvim
+    "epwalsh/obsidian.nvim",
+    ebabled = true,
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      ui = { enable = false },
+      workspaces = {
+        {
+          name = "notes",
+          path = "~/notes",
+        },
+      },
+    },
+  },
 }
